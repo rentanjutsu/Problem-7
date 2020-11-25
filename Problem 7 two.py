fc450cce727a178273ee599119eab0e60ef3350d
rentanjutsu <74683663+rentanjutsu@users.noreply.github.com>
2020-11-25 16:24:25 -0500

    Update Problem 7 two.py

--- Problem 7 two.py
+++ Problem 7 two.py
@@ -1,24 +1,24 @@
 num = int(input())
 wordList = []
 
-    # This is so that the program knows how many tweets exist
-    for line in range(num):
-        tweet = input()
-        tweet = tweet.casefold()
-    # stAdd = start adding
-        stAdd = False
-        word = ''
-        for letter in tweet:
-            if letter == '#':
-                stAdd = True
-            if stAdd:
-                word = word + letter
-                print(word)
-            if letter == ' ':
-                stAdd = False
-                wordList.append(word)
-                word = ''
+# This is so that the program knows how many tweets exist
+for line in range(num):
+    tweet = input()
+    tweet = tweet.casefold()
+# stAdd = start adding
+    stAdd = False
+    word = ''
+    for letter in tweet:
+        if letter == '#':
+            stAdd = True
         if stAdd:
+            word = word + letter
+            print(word)
+        if letter == ' ':
+            stAdd = False
             wordList.append(word)
+            word = ''
+    if stAdd:
+        wordList.append(word)
 
-    print(wordList)
+print(wordList)
