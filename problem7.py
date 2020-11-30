951ae0253dc6ee3859d91ed2d69b4b2040f25a34
rentanjutsu <74683663+rentanjutsu@users.noreply.github.com>
2020-11-30 15:22:14 -0500

    Update problem7.py

--- problem7.py
+++ problem7.py
@@ -1,21 +1,19 @@
-num = int(input())
 wordList = []
-for line in range(num):
-    tweet = input()
-# stAdd = stop adding
-    stAdd = False
-    word = ''
-    for letter in tweet:
-        if letter == '#':
-            stAdd = True
-        if stAdd:
-            word = word + letter
-            print(word)
-        if letter == ' ':
-            stAdd = False
-# wordList.append(word)
-            word = ''
+tweet = input()
+# stAdd = start adding
+stAdd = False
+word = ''
+for letter in tweet:
+    if letter == '#':
+        stAdd = True
     if stAdd:
-        wordList.append(word)
+        word = word + letter
+        print(word)
+    if letter == ' ':
+        stAdd = False
+# wordList.append(word)
+        word = ''
+if stAdd:
+    wordList.append(word)
 
 print(wordList)
