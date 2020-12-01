0527ec5d79489c9c74e26081b7d3f2cda421f96e
rentanjutsu <74683663+rentanjutsu@users.noreply.github.com>
2020-12-01 13:04:39 -0500

    Create Problem 7 three.py

---
+++ Problem 7 three.py
@@ -0,0 +1,13 @@
+num = int(input())
+wordList = []
+for line in range(num):
+    tweet = input()
+    blankline = input()
+
+    words = [word[1:] for word in tweet.strip().split(" ") if word[0] == '#']
+    wordList.extend(words)
+
+col = Counter(wordList)
+
+for key, val in col.most_common():
+    print(str(key) + " " + str(val))
