52315cb3bb450523afad78199044a7d4e75f7b8e
rentanjutsu <74683663+rentanjutsu@users.noreply.github.com>
2020-12-07 18:06:22 -0500

    Update Problem 7 three.py

--- Problem 7 three.py
+++ Problem 7 three.py
@@ -3,7 +3,7 @@ from collections import Counter
 num = int(input())
 wordList = []
 for line in range(num):
-    tweet = input()
+    tweet = input().lower()
     blankLine = input()
 
     words = [word[1:] for word in tweet.strip().split(" ") if word[0] == '#']
@@ -13,3 +13,6 @@ col = Counter(wordList)
 
 for key, val in col.most_common():
     print(str(key) + " " + str(val))
+
+# numList = [i for i in range(100)]
+# key, value = [1, 2]
